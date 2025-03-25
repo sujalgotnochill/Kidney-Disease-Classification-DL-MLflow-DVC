3282a8ae0b069fccea49e80a1b8f2e6ab63c6aad
Sujal Vishwakarma <sujalvishwakarma5@gmail.com>
2025-03-26 00:46:16 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from KidneydiseaseClassifier import logger
 from KidneydiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from KidneydiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -10,3 +12,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
